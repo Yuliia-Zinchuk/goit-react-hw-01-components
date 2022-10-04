740502b3166bd78fc59edb8447019071a05e723a
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-04 17:08:21 +0300

    add task3

---
+++ src/components/FriendList/FriendList.jsx
@@ -0,0 +1,10 @@
+import { FriendListItem } from './FriendListItem';
+export const FriendList = ({ friends }) => {
+  return (
+    <>
+      <ul className="friend-list">
+        <FriendListItem friends={friends} />
+      </ul>
+    </>
+  );
+};
---
+++ src/components/FriendList/FriendListItem.jsx
@@ -0,0 +1,21 @@
+import PropTypes from 'prop-types';
+export const FriendListItem = ({ friends }) => {
+  return friends.map(({ id, avatar, name, isOnline }) => (
+    <li className="item" key={id}>
+      <span className="status">{isOnline}</span>
+      <img className="avatar" src={avatar} alt="User avatar" width="48" />
+      <p className="name">{name}</p>
+    </li>
+  ));
+};
+
+FriendListItem.propTypes = {
+  friends: PropTypes.arrayOf(
+    PropTypes.shape({
+      id: PropTypes.number.isRequired,
+      avatar: PropTypes.string.isRequired,
+      name: PropTypes.string.isRequired,
+      isOnline: PropTypes.bool.isRequired,
+    }).isRequired
+  ),
+};
