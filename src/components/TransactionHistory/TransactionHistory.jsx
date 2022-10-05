5d340ddaf7773cba2fc11322ca1e281bce50792c
Yuliia-Zinchuk <yuliya_zinchuk@icloud.com>
2022-10-05 05:12:12 +0300

    add TransactionHistory.module.css

--- src/components/TransactionHistory/TransactionHistory.jsx
+++ src/components/TransactionHistory/TransactionHistory.jsx
@@ -1,24 +1,27 @@
 import PropTypes from 'prop-types';
+import css from './TransactionHistory.module.css';
 export const TransactionHistory = ({ items }) => {
   return (
-    <table className="transaction-history">
-      <thead>
-        <tr>
-          <th>Type</th>
-          <th>Amount</th>
-          <th>Currency</th>
-        </tr>
-      </thead>
-      <tbody>
-        {items.map(({ id, type, amount, currency }) => (
-          <tr key={id}>
-            <td>{type}</td>
-            <td>{amount}</td>
-            <td>{currency}</td>
+    <section className={css.container}>
+      <table className={css.transactionHistory}>
+        <thead>
+          <tr className={css.tableHeader}>
+            <th className={css.tableHeaderItem}>Type</th>
+            <th className={css.tableHeaderItem}>Amount</th>
+            <th className={css.tableHeaderItem}>Currency</th>
           </tr>
-        ))}
-      </tbody>
-    </table>
+        </thead>
+        <tbody className={css.tableBody}>
+          {items.map(({ id, type, amount, currency }) => (
+            <tr className={css.tableRow} key={id}>
+              <td className={css.tableRowItem}>{type}</td>
+              <td className={css.tableRowItem}>{amount}</td>
+              <td className={css.tableRowItem}>{currency}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
+    </section>
   );
 };
 
